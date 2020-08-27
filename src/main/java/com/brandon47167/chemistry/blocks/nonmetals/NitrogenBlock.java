45ed8b44db20c359ff95828ff92a4133f9c15814
brandon47167 <superyoshifan101@gmail.com>
2020-08-27 08:50:31 -0700

    Nitrogen block is actually good.

--- src/main/java/com/brandon47167/chemistry/blocks/nonmetals/NitrogenBlock.java
+++ src/main/java/com/brandon47167/chemistry/blocks/nonmetals/NitrogenBlock.java
@@ -1,8 +1,21 @@
 package com.brandon47167.chemistry.blocks.nonmetals;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CactusBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ToolType;
+
+import java.util.Random;
 
 public class NitrogenBlock extends Block{
     public NitrogenBlock() {
@@ -13,4 +26,20 @@ public class NitrogenBlock extends Block{
                 .harvestTool(ToolType.PICKAXE)
         );
     }
+
+
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        if (rand.nextInt(5) == 0) {
+            for (int i = 0; i < rand.nextInt(1) + 1; ++i) {
+                worldIn.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, (double) ((float) pos.getX() + 0.5F), (double) ((float) pos.getY() + 0.5F), (double) ((float) pos.getZ() + 0.5F), (double) (rand.nextFloat() / 2.0F), 5.0E-5D, (double) (rand.nextFloat() / 2.0F));
+            }
+        }
+
+    }
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+        entityIn.attackEntityFrom(DamageSource.GENERIC, 2.0F);
+        super.onEntityWalk(worldIn, pos, entityIn);
+    }
+
 }
